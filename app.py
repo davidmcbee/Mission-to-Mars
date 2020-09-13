27ecbc98ff751e247e79b9b3de71263950b6ed72
unknown <davidmcbee8@gmail.com>
2020-09-13 12:59:40 -0700

    Everything working

--- app.py
+++ app.py
@@ -19,11 +19,10 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+   
 
-# Update mongodb
-mars.update({}, mars_data, upsert=True)
-return "Scraping successful!"
+   
+   return "Scraping successful!"
 
 if __name__ == "__main__":
    app.run()
